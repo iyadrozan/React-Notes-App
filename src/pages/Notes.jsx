5bc8640572f58d7b2b9d2c57aeafdc3e2be2174a
iyadrozan <iyadrozan26@gmail.com>
2023-03-20 08:56:10 +0800

    add dummy_notes.js

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { CiSearch } from "react-icons/ci";
+import { BsPlusLg } from "react-icons/bs";
+import dummyNotes from "../dummy_notes";
+import { Link } from "react-router-dom";
+import NoteItem from "../components/NoteItem";
 
 const Notes = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <section>
+      <header className="notes__header">
+        <h2>My Notes</h2>
+        {/* <input type="text" autoFocus placeholder="Keyword..." /> */}
+        <button className="btn">
+          <CiSearch />
+        </button>
+      </header>
+      <div className="notes__container">
+        {dummyNotes.map((note) => (
+          <NoteItem key={note.id} note={note} />
+        ))}
+      </div>
+      <Link className="btn add__btn">
+        <BsPlusLg />
+      </Link>
+    </section>
   );
 };
 
